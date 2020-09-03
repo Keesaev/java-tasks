65e0767d863696deaf3ded0ff6a7b9e035677044
keesaev <kostia.234@mail.ru>
2020-09-03 21:41:21 +0300

    task1

--- task1/task1.java
+++ task1/task1.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class task1{
     public static void main(String []args) {
-		int a = 17;
-		int b = 10;
-        System.out.println(3 % 4); 
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("Type 2 numbers");
+		int a = in.nextInt();
+		int b = in.nextInt();
+
+		System.out.println(a + " % " + b + " = " + a % b);
     }
 }
